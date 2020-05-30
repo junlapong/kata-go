006b97dbfec67935d8d23678688bd7904a00e725
Junlapong L <junlapong@gmail.com>
2020-05-31 06:50:26 +0700

    add main_test.go

---
+++ main_test.go
@@ -0,0 +1,25 @@
+
+package main
+
+import (
+    "testing"
+    "os"
+    "log"
+)
+
+func TestMain(m *testing.M) {
+    log.Println("Do stuff BEFORE the tests!")
+    exitVal := m.Run()
+    log.Println("Do stuff AFTER the tests!")
+
+    os.Exit(exitVal)
+}
+
+func TestA(t *testing.T) {
+    log.Println("TestA running")
+}
+
+func TestB(t *testing.T) {
+    log.Println("TestB running")
+}
+
